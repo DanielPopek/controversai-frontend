73fc3b1a1e96028609f10dbf4cb5bcca4d6fa2e8
Marcin Wątroba <marcin.watroba@deviniti.com>
2020-01-05 13:35:43 +0100

    Fix CI

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,4 @@
 docker-compose -f projects/political_ly/frontend/docker-compose.yml build --no-cache
 docker-compose -f projects/political_ly/frontend/docker-compose.yml up -d
+ls -l projects/political_ly/frontend/dist
 rm -rf projects/political_ly/frontend/dist
